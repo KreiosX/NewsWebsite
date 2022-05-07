db3aab01ac34e91945d82ad3a2e75fd4b96e0c7f
KreiosX <103337925+KreiosX@users.noreply.github.com>
2022-05-07 17:56:22 +0200

    Update sqlBaglanti.cs
    CS

--- App_Code/sqlBaglanti.cs
+++ App_Code/sqlBaglanti.cs
@@ -12,7 +12,7 @@ public class sqlBaglanti
 {
     public SqlConnection baglan()
     {
-        SqlConnection baglanti = new SqlConnection("Data Source=79.123.231.145,7857; Network Library=DBMSSOCN;Initial Catalog=2016_2017_gunduz;User Id=intProg;Password=1234;");
+        SqlConnection baglanti = new SqlConnection("----");
         baglanti.Open();
         SqlConnection.ClearPool(baglanti);
         SqlConnection.ClearAllPools();
